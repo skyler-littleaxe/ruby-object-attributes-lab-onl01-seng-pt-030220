1f8a3ba0bc38f095e9a4617aa46347339dc24438
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-18 03:05:42 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def name
+    @name 
+  end
+  
+  def name=(p_name)
+    @name = p_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(p_job)
+    @job = p_job
+  end
+
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = ""
